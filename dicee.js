cde06c9dc29a2cb29627453372f98e2a3a93a7c2
Wollas Souza <63406778+Wollaz@users.noreply.github.com>
2021-05-31 03:50:21 -0300

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,5 +1,5 @@
 function diceRoll(dice) {
-    dice = ["/images/dice1.png","/images/dice2.png","/images/dice3.png","/images/dice4.png","/images/dice5.png","/images/dice6.png"];
+    dice = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
     dice = dice[Math.floor(Math.random() * dice.length)] ;
     return dice;
 }
